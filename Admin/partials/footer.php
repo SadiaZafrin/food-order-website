00d9658f919caaeea511d7972fbdc83ad8f4b3e9
SadiaZafrin <137418863+SadiaZafrin@users.noreply.github.com>
2024-11-09 01:08:45 +0600

    Add files via upload

---
+++ Admin/partials/footer.php
@@ -0,0 +1,11 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2020 All rights reserved, Food House. Developed By - <a href="#"> Data Pirates </a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
+<!-- footer section ends -->
